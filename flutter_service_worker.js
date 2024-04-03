3ab72abb98f1a6da65cfa4749d65457c4316b568
viodrz <165645314+Vio-Fatin-20-04-24@users.noreply.github.com>
2024-04-03 14:54:40 +0700

    sync

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "4fa301afbb24efa9fbe54b0fc3db44f7",
-"index.html": "7b4fd0ec4988633fc21cc2fb74ddaa41",
-"/": "7b4fd0ec4988633fc21cc2fb74ddaa41",
-"main.dart.js": "5bd12ee303af01a03ac4f8dacd8b751a",
+"index.html": "1a5e083c0c06bd88d7b8c420ede437f8",
+"/": "1a5e083c0c06bd88d7b8c420ede437f8",
+"main.dart.js": "8ca5afd075158461003a7daa4109de73",
 "flutter.js": "c71a09214cb6f5f8996a531350400a9a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
